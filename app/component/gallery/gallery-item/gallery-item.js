b7854e454c5c749f42eb35285d99d766627bcb94
Bhavya Bharti <bhavyab@outlook.com>
2017-01-31 12:49:30 -0800

    mvp data binding

---
+++ app/component/gallery/gallery-item/gallery-item.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const module = require('./_gallery-item.scss');
+
+module.exports = {
+  tamplate: require('./gallery-item.html'),
+  controller: ['$log', 'galleryService', GalleryItemController],
+  controllerAs: 'GalleryItemCtrl',
+  bindings: {
+    gallery: '<'
+  }
+};
+
+function GalleryItemController($log, galleryService){
+  $log.debug('GalleryItemController');
+
+  this.showEditGallery = false;
+
+  this.deleteGallery = function(){
+    galleryService.deleteGallery(this.gallery._id);
+  };
+}
